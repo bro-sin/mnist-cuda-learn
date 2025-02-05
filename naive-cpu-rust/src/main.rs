8c099b125ed4c3203432d803fded5560b881aa2b
bro_SiN <bro.sin@qq.com>
2025-02-05 22:27:32 +0800

    增加代码可读性

--- naive-cpu-rust/src/main.rs
+++ naive-cpu-rust/src/main.rs
@@ -1,5 +1,11 @@
+use rand::Rng;
 use std::fs;
 use std::io::{self, Read};
+
+const PICTURE_WIDTH: usize = 28;
+const PICTURE_HEIGHT: usize = 28;
+const PICTURE_SIZE: usize = PICTURE_WIDTH * PICTURE_HEIGHT;
+const LABEL_SIZE: usize = 1;
 
 struct DataSet {
     images_file_path: String,
@@ -11,7 +17,7 @@ struct DataSet {
 impl DataSet {
     fn load_images_data(&mut self) -> io::Result<()> {
         let mut data_bin_file = fs::File::open(&self.images_file_path)?;
-        let mut buffer = vec![0u8; self.num_elements * 4 * 28 * 28];
+        let mut buffer = vec![0u8; self.num_elements * std::mem::size_of::<f32>() * PICTURE_SIZE];
         data_bin_file.read_exact(&mut buffer)?;
         let data: Vec<f32> = buffer
             .chunks(4)
@@ -23,46 +29,41 @@ impl DataSet {
 
     fn load_labels_data(&mut self) -> io::Result<()> {
         let mut data_bin_file = fs::File::open(&self.labels_file_path)?;
-        let mut buffer = vec![0u8; self.num_elements * std::mem::size_of::<i32>()];
+        let mut buffer = vec![0u8; self.num_elements * std::mem::size_of::<i32>() * LABEL_SIZE];
         data_bin_file.read_exact(&mut buffer)?;
         let data: Vec<i32> = buffer
             .chunks_exact(std::mem::size_of::<i32>())
-            .map(|chunk| {
-                let mut tmp = [0u8; 4];
-                tmp.copy_from_slice(chunk);
-                i32::from_le_bytes(tmp)
-            })
+            .map(|chunk| i32::from_le_bytes([chunk[0], chunk[1], chunk[2], chunk[3]]))
             .collect();
         self.labels_data = Some(data);
         Ok(())
     }
     fn load_data(&mut self) -> io::Result<()> {
-        self.load_images_data();
-        self.load_labels_data();
+        self.load_images_data()?;
+        self.load_labels_data()?;
         if let (Some(images_data), Some(labels_data)) = (&self.images_data, &self.labels_data) {
-            print!(
-                "Data loaded, images length: {}, labels length: {}.\n",
+            println!(
+                "Data loaded, images length: {}, labels length: {}.",
                 { images_data.len() },
                 { labels_data.len() }
             );
         } else {
-            print!("Data load failed!");
+            println!("Data load failed!");
         }
         Ok(())
     }
 
     fn show(&self, index: usize) {
+        println!("The index is: {}", { index });
         if let Some(labels_data) = &self.labels_data {
-            // for i in 0..labels_data.len() {
-            //     println!("labels[{}]:{}", { i }, { labels_data[i] });
-            // }
-            print!("The picture of {} is as follows:\n", { labels_data[index] });
+            println!("The picture of {} is as follows:", { labels_data[index] });
+        } else {
+            println!("There is no picture");
         }
-        //在终端显示第一张图片
         if let Some(images_data) = &self.images_data {
-            for i in 0..28 {
-                for j in 0..28 {
-                    if images_data[index * 28 * 28 + i * 28 + j] > 0f32 {
+            for i in 0..PICTURE_HEIGHT {
+                for j in 0..PICTURE_WIDTH {
+                    if images_data[index * PICTURE_SIZE + i * PICTURE_WIDTH + j] > 0f32 {
                         print!("x");
                     } else {
                         print!(" ");
@@ -70,7 +71,14 @@ impl DataSet {
                 }
                 print!("\n");
             }
+        } else {
+            println!("There is no picture");
         }
+    }
+    fn show_random(&self) {
+        let mut rng = rand::rng();
+        let index = rng.random_range(0..self.num_elements);
+        self.show(index);
     }
 }
 
@@ -82,7 +90,7 @@ fn main() -> io::Result<()> {
         images_data: None,
         labels_data: None,
     };
-    train_dataset.load_data();
-    train_dataset.show(89);
+    train_dataset.load_data()?;
+    train_dataset.show_random();
     Ok(())
 }
